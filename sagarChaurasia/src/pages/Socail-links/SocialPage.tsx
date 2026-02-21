072eb5c86de33c4fbfb81aa5f0f7197a21768ecd
sagarchaurasia176 <sagar.volgainfosys@gmail.com>
2026-02-21 12:41:08 +0530

    fix:resume url fixed

--- sagarChaurasia/src/pages/Socail-links/SocialPage.tsx
+++ sagarChaurasia/src/pages/Socail-links/SocialPage.tsx
@@ -33,7 +33,7 @@ export default function SocialPage() {
             <span className="font-inter">+91 7988595603</span>
           </a>
           <a
-            href="https://drive.google.com/file/d/1XIFJGpiJUVb4aQRwbc5dKObrg4OTqZMj/view?usp=sharing"
+            href="https://drive.google.com/file/d/17yq35LprNPdwO_RR7H1nba_8QcJ7fq4X/view?usp=sharing"
             target="_blank"
             className="flex items-center gap-4 text-slate-700 hover:text-purple-600 transition-all duration-300 font-medium hover:bg-slate-50 p-2 rounded-lg"
           >
